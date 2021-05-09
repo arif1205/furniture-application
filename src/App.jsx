490767c39b148519eede5844ba8db24fa74287c4
arif1205 <imdariful71@gmail.com>
2021-05-09 07:48:02 +0600

    starting my project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div>
+			<h1>App is running...</h1>
+			<i class='fas fa-address-book'></i>
+		</div>
+	);
+}
+
+export default App;
